c1935c500bb644fe08e071a8378aab3248cfb3fe
patrick <thangphung1310@gmail.com>
2021-12-27 00:01:51 +0700

    Update Calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -58,7 +58,7 @@ export default class Calculator extends React.Component {
 
     render() {
         return (
-            <div className="bg-indigo-200 rounded-3xl p-12">
+            <div className="rounded-3xl p-12">
                 <div className="mb-8">
                     <h1 className="text-3xl">Mortgage Calculator</h1>
                 </div>
